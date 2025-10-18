32b4124fb050124dd5d3a254aaa67bc48b64eb23
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-10-18 18:57:03 +0530

    Update

---
+++ server/controllers/role.controller.js
@@ -0,0 +1,12 @@
+const RoleController = {
+    createRole: async(req, res) => {
+        try{
+
+        }
+        catch(err){
+            
+        }
+    }
+};
+
+module.exports = RoleController;

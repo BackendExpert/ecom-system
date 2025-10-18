827bc22eba00048120f8693d8185cdfb3b827fe3
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-10-18 13:27:23 +0530

    Create role.service.js

---
+++ server/services/role.service.js
@@ -0,0 +1,51 @@
+const jwt = require("jsonwebtoken")
+
+const logUserAction = require("../utils/others/logUserAction")
+
+const Role = require("../models/role.model");
+
+const {
+    CreateRoleResDTO
+} = require("../dtos/role.dto");
+
+
+class RoleService {
+    static async createRole(token, rolename, req) {
+        let decoded;
+        try {
+            decoded = jwt.verify(token, process.env.JWT_SECRET);
+        } catch (err) {
+            if (err.name === "TokenExpiredError") {
+                throw new Error("Token expired. Please request a new one.");
+            }
+            throw new Error("Invalid token.");
+        }
+
+        const checkrole = await Role.findOne({ name: rolename })
+
+        if (checkrole) {
+            throw new Error("Role Already Exist")
+        }
+
+        const newRole = new Role({
+            name: rolename,
+        })
+
+        const resultCreateRole = await newRole.save()
+
+        if (resultCreateRole) {
+            if (req) {
+                const metadata = {
+                    ipAddress: req.headers['x-forwarded-for'] || req.socket.remoteAddress,
+                    userAgent: req.headers['user-agent'],
+                    timestamp: new Date(),
+                };
+                await logUserAction(req, "role_created", `${decoded.email} Create Role ${rolename}`, metadata, user._id);
+            }
+
+            return CreateRoleResDTO()
+        }
+    }
+}
+
+module.exports = RoleService

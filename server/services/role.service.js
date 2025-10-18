c435acf1b1e9ac1b5e54f1ecbe5b5720267f0206
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-10-18 13:32:59 +0530

    Update role.service.js

--- server/services/role.service.js
+++ server/services/role.service.js
@@ -6,7 +6,10 @@ const Role = require("../models/role.model");
 const User = require("../models/user.model")
 
 const {
-    CreateRoleResDTO
+    CreateRoleResDTO,
+    CreatePremissionResDTO,
+    GetRoleResDTO,
+    GetPermissionForResRole
 } = require("../dtos/role.dto");
 
 
@@ -50,7 +53,7 @@ class RoleService {
         }
     }
 
-    static async createPermission(token, roleid, perName) {
+    static async createPermissions(token, roleid, permissions, req) {
         let decoded;
         try {
             decoded = jwt.verify(token, process.env.JWT_SECRET);
@@ -64,7 +67,54 @@ class RoleService {
         const user = await User.findOne({ email: decoded.email });
         if (!user) throw new Error("User not found");
 
-        
+        const permissionsArray = Array.isArray(permissions) ? permissions : [permissions];
+
+        const getrole = await Role.findById(roleid);
+        if (!getrole) throw new Error("Role not found");
+
+        const existingPermissions = getrole.permissions.map(p => p.toString());
+        const newPermissions = permissionsArray.filter(p => !existingPermissions.includes(p));
+
+        if (newPermissions.length === 0) {
+            return {
+                success: false,
+                error: "No new permissions were added."
+            };
+        }
+
+        const permissionCreate = await Role.findByIdAndUpdate(
+            roleid,
+            { $addToSet: { permissions: { $each: newPermissions } } },
+            { new: true, runValidators: true }
+        );
+
+        if (permissionCreate && req) {
+            const metadata = {
+                ipAddress: req.headers['x-forwarded-for'] || req.socket.remoteAddress,
+                userAgent: req.headers['user-agent'],
+                timestamp: new Date(),
+            };
+            await logUserAction(
+                req,
+                "new_permission_create",
+                `${decoded.email} new Permission Created for ${getrole.name}`,
+                metadata,
+                user._id
+            );
+        }
+
+        return CreatePremissionResDTO()
+
+    }
+
+    static async getallroles(){
+        const getroles = await Role.find()
+        return GetRoleResDTO()
+    }
+
+    static async getpermissions(roleid){
+        const getpermission = await Role.findById(roleid)
+        return GetPermissionForResRole()
     }
 }
 

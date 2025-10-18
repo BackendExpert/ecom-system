7ad302528203092037e1567943c354dbbdcc4cb6
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-10-18 19:18:37 +0530

    Update role.controller.js

--- server/controllers/role.controller.js
+++ server/controllers/role.controller.js
@@ -1,10 +1,34 @@
-const RoleController = {
-    createRole: async(req, res) => {
-        try{
+const {
+    RoleErrorResDTO,
+    CreateRoleDTO
+} = require("../dtos/role.dto");
+const RoleService = require("../services/role.service");
 
+const RoleController = {
+    createRole: async (req, res) => {
+        try {
+
+            const token = req.header("Authorization")?.replace("Bearer ", "");
+            if (!token) {
+                return res.status(401).json({ message: "Access denied. No token provided." });
+            }
+
+            const {
+                rolename
+            } = req.body
+
+            const roledto = CreateRoleDTO(rolename, token)
+
+            const result = await RoleService.createRole(
+                roledto.token,
+                roledto.name,
+                req
+            )
+
+            res.status(200).json(result)
         }
-        catch(err){
-            
+        catch (err) {
+            return res.status(400).json(RoleErrorResDTO(err.message));
         }
     }
 };

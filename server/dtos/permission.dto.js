22bd8ea4df048b13a2910922a60b9810b5e08357
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-10-18 12:07:23 +0530

    Update permission.dto.js

--- server/dtos/permission.dto.js
+++ server/dtos/permission.dto.js
@@ -1,7 +1,16 @@
 exports.CreatePermissionDTO = (
-
+    name,
+    desc,
+    token
 ) => ({
-
+    name,
+    desc,
+    token
 })
 
 exports.CreatePermissionResDTO = (message = "Permission Created Successfully") => ({ success: true, message})
+
+
+exports.GrantPermissionToUserDTO = (token, userid, permissionid) => ({ token, userid, permissionid })
+
+exports.GrantPermissionToUserResDTO = (message="Permission Grant to User Successfully") => ({ success: true, message })

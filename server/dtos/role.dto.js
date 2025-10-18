2b9432dd17d40b20bf4b97cce4fdeb95e4ff8339
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-10-18 13:21:08 +0530

    Update

---
+++ server/dtos/role.dto.js
@@ -0,0 +1,12 @@
+exports.CreateRoleDTO = (token, name) => ({ token, name })
+exports.CreateRoleResDTO = (message = "Role Created Successfully") => ({ success: true, message })
+
+exports.CreatePremissionDTO = (token, roleid, perName) => ({ token, roleid, perName })
+exports.CreatePremissionResDTO = (message = "Permission Added to Role Successfully") => ({ success: true, message})
+
+exports.GetRoleResDTO = (result, message="All Roles Fetched Successfully") => ({ success: true, result, message })
+
+exports.GetPermissionForRole = (token, roleid) => ({ token, roleid })
+exports.GetPermissionForResRole = (result, message="All Permissions For Role Fetched Successfully") => ({ success: true, result, message})
+
+exports.RoleErrorResDTO = (message="Something went wrong") => ({ success: false, message})

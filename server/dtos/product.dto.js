56109edd8fae63162793e1d8fea84111132f21f2
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-10-19 08:00:10 +0530

    Update product.dto.js

--- server/dtos/product.dto.js
+++ server/dtos/product.dto.js
@@ -1,27 +1,107 @@
 exports.CreateBrandDTO = (token, brandName) => ({ token, brandName })
-exports.CreateBrandResDTO = (message="Brand Created Successfully") => ({ success: true, message })
+exports.CreateBrandResDTO = (message = "Brand Created Successfully") => ({ success: true, message })
 
-exports.GetAllBrandsResDTO = (result, message="All Brands Fetched Successfully") => ({ success: true, result, message })
+exports.GetAllBrandsResDTO = (result, message = "All Brands Fetched Successfully") => ({ success: true, result, message })
 
 exports.GetOneBrandDTO = (token, brandid) => ({ token, brandid })
-exports.GetOneBrandResDTO = (result, message="One Brand Fetched Successfully") => ({ success: true, result, message})
+exports.GetOneBrandResDTO = (result, message = "One Brand Fetched Successfully") => ({ success: true, result, message })
 
-exports.CreateProductTypeDTO = (token, ptName ) => ({ token, ptName })
-exports.CreateProductTypeResDTO = (message="Product Type Created Successfully") => ({ success: true, message })
+exports.CreateProductTypeDTO = (token, ptName) => ({ token, ptName })
+exports.CreateProductTypeResDTO = (message = "Product Type Created Successfully") => ({ success: true, message })
 
-exports.GetAllProductTypesResDTP = (result, message="All Product Types Fetched Successfully") => ({ success: true, result, message })
+exports.GetAllProductTypesResDTP = (result, message = "All Product Types Fetched Successfully") => ({ success: true, result, message })
 
-exports.GetOneProductTypeDTO = (token, ptid) => ({ token, ptid})
-exports.GetOneProductTypeResDTO = (result, message="One Product Types Fetched Successfully") => ({ success: true, result, message })
+exports.GetOneProductTypeDTO = (token, ptid) => ({ token, ptid })
+exports.GetOneProductTypeResDTO = (result, message = "One Product Types Fetched Successfully") => ({ success: true, result, message })
 
 exports.CreateTagDTO = (token, tagName) => ({ token, tagName })
-exports.CreateTagResDTO = (message="Product Tag Created Successfully") => ({ success: true, message })
+exports.CreateTagResDTO = (message = "Product Tag Created Successfully") => ({ success: true, message })
 
-exports.GetAllTagsResDTO = (result, message="All Product Tags Fetched Successfully") => ({ success: true, result, message })
+exports.GetAllTagsResDTO = (result, message = "All Product Tags Fetched Successfully") => ({ success: true, result, message })
 
 exports.GetOneTagDTO = (token, tagid) => ({ token, tagid })
-exports.GetOneTagResDTO = (result, message="One Product Tags Fetched Successfully") => ({ success: true, result, message })
+exports.GetOneTagResDTO = (result, message = "One Product Tags Fetched Successfully") => ({ success: true, result, message })
 
 exports.CreateCategoryDTO = (token, catName, catDesc) => ({ token, catName, catDesc })
-exports.CreateCategoryResDTO = (result, message="Product Category Created Successfully") => ({ success: true, message })
+exports.CreateCategoryResDTO = (message = "Product Category Created Successfully") => ({ success: true, message })
+
+exports.GetAllCategoryResDTO = (result, message = "All Product Category Fetched Successfully") => ({ success: true, result, message })
+
+exports.GetOneCategoryDTO = (token, catID) => ({ token, catID })
+exports.GetOneCategoryResDTO = (result, message = "One Product Category Fetched Successfully") => ({ success: true, result, message })
+
+exports.CreateProductDTO = (
+    token,
+    itemCode,
+    productName,
+    regularPrice,
+    salePrice,
+    size,
+    stock,
+    SKU,
+    categoryid,
+    tagsid,
+    description,
+    images,
+    productTypeids,
+    brandid,
+    netWeight
+) => ({
+    token,
+    itemCode,
+    productName,
+    regularPrice,
+    salePrice,
+    size,
+    stock,
+    SKU,
+    categoryid,
+    tagsid,
+    description,
+    images,
+    productTypeids,
+    brandid,
+    netWeight
+})
+
+exports.CreateProductResDTO = (message = "Product Category Created Successfully") => ({ success: true, message })
+
+exports.UpdateProductDTO = (
+    token,
+    itemCode,
+    productName,
+    regularPrice,
+    salePrice,
+    size,
+    stock,
+    SKU,
+    categoryid,
+    tagsid,
+    description,
+    images,
+    productTypeids,
+    brandid,
+    netWeight
+) => ({
+    token,
+    itemCode,
+    productName,
+    regularPrice,
+    salePrice,
+    size,
+    stock,
+    SKU,
+    categoryid,
+    tagsid,
+    description,
+    images,
+    productTypeids,
+    brandid,
+    netWeight
+})
+
+exports.GetAllProductsResDTO = (result, message="All Products Fetched Successfully") => ({ success: true, result, message })
+
+exports.GetOneProductDTO = (token, producutID) => ({ token, producutID })
+exports.GetOneProductResDTO = (result, message="One Product Fetched Successfully") => ({ success: true, result, message })
 

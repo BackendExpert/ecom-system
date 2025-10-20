8c31a0fb2bd46834571c1cfdbea06b73f5e538a8
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-10-20 06:52:24 +0530

    Update product.service.js

--- server/services/product.service.js
+++ server/services/product.service.js
@@ -4,10 +4,14 @@ const logUserAction = require("../utils/others/logUserAction")
 const User = require("../models/user.model")
 const Brand = require("../models/brand.model");
 const ProductType = require("../models/productType.model")
+const Tag = require("../models/tag.model")
 
-const { 
+const {
     CreateBrandResDTO,
-    CreateProductTypeResDTO
+    CreateProductTypeResDTO,
+    GetAllBrandsResDTO,
+    GetAllProductTypesResDTP,
+    CreateTagResDTO
 } = require("../dtos/product.dto");
 
 class ProductService {
@@ -82,12 +86,60 @@ class ProductService {
                     userAgent: req.headers['user-agent'],
                     timestamp: new Date(),
                 };
-                await logUserAction(req, "brand_created", `${decoded.email} Create Brand`, metadata, user._id);
+                await logUserAction(req, "product_type-_create", `${decoded.email} Create product type`, metadata, user._id);
             }
 
             return CreateProductTypeResDTO()
         }
     }
+
+    static async GetAllBrands() {
+        const allbrands = await Brand.find()
+        return GetAllBrandsResDTO()
+    }
+
+    static async GetAllProductTypes() {
+        const allproducttype = await ProductType.find()
+        return GetAllProductTypesResDTP()
+    }
+
+    static async CreateProductTag(token, name, req) {
+        let decoded;
+        try {
+            decoded = jwt.verify(token, process.env.JWT_SECRET);
+        } catch (err) {
+            if (err.name === "TokenExpiredError") {
+                throw new Error("Token expired. Please request a new one.");
+            }
+            throw new Error("Invalid token.");
+        }
+
+        const user = await User.findOne({ email: decoded.email });
+        if (!user) throw new Error("User not found");
+
+        const checktag = await Tag.findOne({ name: name })
+        if (checktag) throw new Error("Tag Already Exist");
+
+        const newTag = new Tag({
+            name: name,
+        })
+
+        const resultcreateTag = await newTag.save()
+
+        if (resultcreateTag) {
+            if (req) {
+                const metadata = {
+                    ipAddress: req.headers['x-forwarded-for'] || req.socket.remoteAddress,
+                    userAgent: req.headers['user-agent'],
+                    timestamp: new Date(),
+                };
+                await logUserAction(req, "product_tag_create", `${decoded.email} Create product Tag`, metadata, user._id);
+            }
+
+            return CreateTagResDTO()
+        }
+    }
+
 }
 
 module.exports = ProductService
